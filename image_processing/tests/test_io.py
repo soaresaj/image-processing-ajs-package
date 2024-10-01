73fdf1af164c4c1041f5ba7d16c6e1ad7d7910f1
soaresaj <61300133+soaresaj@users.noreply.github.com>
2024-09-30 21:55:12 -0300

    Update test_io.py
    Alterar implementação dos testes dos módulos io.py

--- image_processing/tests/test_io.py
+++ image_processing/tests/test_io.py
@@ -1,9 +1,9 @@
 import pytest
-from image_processing_ajs.io import load_image, save_image
 import numpy as np
+from image_processing_ajs.io import load_image, save_image
 
 def test_load_image():
-    # Tests if the image is loaded correctly
+    # Test if the image loads correctly
     image = load_image('path/to/test_image.jpg')
     assert image is not None
     assert isinstance(image, np.ndarray)
@@ -11,4 +11,4 @@ def test_load_image():
 def test_save_image():
     test_image = np.zeros((100, 100, 3))  # Create a test image
     result = save_image('saved_image.jpg', test_image)
-    assert result is True  # Checks if the image was saved successfully
+    assert result is True  # Check if the image was saved successfully
